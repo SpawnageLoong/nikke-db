b76ae0305d3d09799631d678e975bc45a7fd66d9
SpawnageLoong <richardloongcj@gmail.com>
2024-10-30 22:13:47 +0800

    Add ts types for characters and move characters.json

--- src/app.d.ts
+++ src/app.d.ts
@@ -11,3 +11,21 @@ declare global {
 }
 
 export {};
+
+export type Skill = {
+	name: string;
+	description: string;
+}
+
+export type Character = {
+	id: string;
+	name: string;
+	rarity: string;
+	burst: number
+	element: string;
+	weapon: string;
+	type: string;
+	manufacturer: string;
+	squad: string;
+	tags: string[];
+}
